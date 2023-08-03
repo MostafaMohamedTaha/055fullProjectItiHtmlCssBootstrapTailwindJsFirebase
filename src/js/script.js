52df76ef97aed92e320a653de08ecf542e782cc1
MostafaMohamedTaha <88216249+MostafaMohamedTaha@users.noreply.github.com>
2023-08-03 23:06:02 +0300

    add user page prepare

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 // Script for navigation bar
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
-const nav= document.getElementById('navber');
+const nav= document.getElementById('navbar');
 
 if(bar){
     bar.addEventListener('click',()=>{
